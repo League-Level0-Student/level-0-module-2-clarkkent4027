d2734f6738046b4b2b1af7d104c8b23fa39cff93
League Student <league@iMac-17.attlocal.net>
2019-10-06 15:56:21 -0700

    Lottery Tickatts

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -17,9 +17,21 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+if (randomNumber == 0) {
+	JOptionPane.showMessageDialog(null, "Your name is Parry Hotter");
+}else if (randomNumber == 1)  {
+	JOptionPane.showMessageDialog(null, "Your name is Tonald Drump");
+}else if (randomNumber == 2)  {
+	JOptionPane.showMessageDialog(null, "Your name is Gelena Somez");
+}else if (randomNumber == 3)  {
+	JOptionPane.showMessageDialog(null, "Your name is Bharlie Crown");
+}else if (randomNumber == 4)  {
+	JOptionPane.showMessageDialog(null, "Your name is Lennifer Jopez");
+}else if (randomNumber == 5)  {
+	JOptionPane.showMessageDialog(null, "Your name is Bustin Jieber");
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
+}
 	}
 }
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -6,16 +6,23 @@ package _05_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
+int jeff = 0;
+Random number = new Random();
+jeff = number.nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(jeff);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog("Please ask a question for the Magic 8 Ball");
 	// 5. If the random number is 0
 
 	// -- tell the user "Yes"
@@ -31,5 +38,16 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
+if (jeff == 0) {
+	JOptionPane.showMessageDialog(null, "Yes");
+}else if (jeff == 1)  {
+	JOptionPane.showMessageDialog(null, "No");
+}else if (jeff == 2)  {
+	JOptionPane.showMessageDialog(null, "Try Again");
+}else if (jeff == 3)  {
+	JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
 
+
+}
+}
 }
